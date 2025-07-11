8f3b0fbe6286e3e4efba617d10ca459e9f587d63
Mushfique Tajwar <mushfique.tajwar.bd@gmail.com>
2025-07-11 22:01:27 +0600

    Commit

--- backend/middleware/notFound.js
+++
@@ -1,9 +0,0 @@
-const notFound = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404).json({
-    success: false,
-    message: `Route ${req.originalUrl} not found`
-  });
-};
-
-module.exports = notFound;
